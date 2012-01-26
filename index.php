ff7b6219995a62e557f4e8f86028d2624c310a06
Dave Olsen <dmolsen@gmail.com>
2012-01-26 15:50:24 -0500

    clarified what should get sent to me if a test doesn't work

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ require("UAParser.php");
 $result = UA::parse();
 
 print "<html><head><title>ua-parser-php</title></head><body><h1>ua-parser-php Test</h1>";
-print "<p><a href=''>ua-parser-php</a> is a pseudo-port of <a href=''>ua-parser</a>. Please use this page to test your browser. ";
-print "If you notice any incorrect information email me at dmolsen+uaparser@gmail.com.</p>";
+print "<p><a href='https://github.com/dmolsen/ua-parser-php'>ua-parser-php</a> is a pseudo-port of <a href='http://code.google.com/p/ua-parser/'>ua-parser</a>. Please use this page to test your browser. ";
+print "If you notice any incorrect information email me at dmolsen+uaparser@gmail.com. Please include the <em>uaOriginal</em> field as well as what you think the browser should be classified as in your message. </p>";
 
 if ($result) {
 	print "<pre>";
